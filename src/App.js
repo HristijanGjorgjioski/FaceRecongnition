874c5f690c3806fb95b5eef79aebe7a2ead1dee4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-21 18:15:33 +0100

    successfully detect faces and show a box around a face!!!

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
 const app = new Clarifai.App({
-  apiKey: 'api',
+  apiKey: 'a5d94266a9474a5aa1da2b85b3489688',
 });
 
 const particleParams = {
@@ -34,8 +34,28 @@ class App extends React.Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box });
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -43,13 +63,19 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
+    // app.models
+    //   .predict(Clarifai.GENERAL_MODEL, this.state.input)
+    //   .then((response) => {
+    //     for (let i = 0; i <= 20; i++) {
+    //       console.log(response.outputs[0].data.concepts[i].name);
+    //     }
+    //   });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      });
+      .then((response) => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -63,7 +89,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
