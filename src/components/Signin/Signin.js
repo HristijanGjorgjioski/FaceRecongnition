c2079fad993c3a715df26ae4642148df136a793d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-24 07:13:09 +0100

    signup form is finally done, now is time to work on signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signin = ({ onRouteChange }) => {
+  const url = 'http://localhost:5000/register';
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+    msg: [],
+  });
+
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
       <main className="pa4 black-80">
