b39fbaf03aa5cd7382d3b49248e2f81dd6fa98be
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-09 10:10:49 +0200

    Created login and signup functions in Context file, now is time to test them

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,6 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
+
+import { auth } from '../firebase'
 
 const AuthContext = React.createContext()
 
@@ -9,8 +11,26 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState()
 
+    const signup = (email, password) => {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    const login = (email, password) => {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setCurrentUser(user)
+        })
+
+        return unsubscribe
+    }, [])
+
     const value = {
-        currentUser
+        currentUser,
+        signup,
+        login
     }
 
     return (
